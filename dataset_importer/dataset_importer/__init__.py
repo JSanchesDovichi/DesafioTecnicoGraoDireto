9384596c40bdc391b2b267ce1d815329486e3ffd
João Pedro Sanches Dovichi <jsanchesdovichi@gmail.com>
2024-11-26 00:41:56 -0300

    Script para importar dataset

---
+++ dataset_importer/dataset_importer/__init__.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from pymongo import MongoClient
+
+# reading csv file 
+df_restaurantes = pd.read_csv("/mnt/LinuxStorage/Projetos/DesafioTecnico/datasets/restaurants.csv")
+df_cardapios = pd.read_csv("/mnt/LinuxStorage/Projetos/DesafioTecnico/datasets/restaurant-menus.csv")
+# print(df_restaurantes.columns.to_list())
+
+myclient = MongoClient("mongodb://root:example@localhost:27017/")
+mydb = myclient["desafio_tecnico"]
+tabela_restaurantes = mydb["restaurantes"]
+
+'''
+mydict = { "name": "John", "address": "Highway 37" }
+
+x = mycol.insert_one(mydict)
+'''
+
+for index, row in df_restaurantes.iterrows():
+    # ['id', 'position', 'name', 'score', 'ratings', 'category', 'price_range', 'full_address', 'zip_code', 'lat', 'lng']
+    # print(row['id'], row['position'])
+
+    cardapio_convertido = []
+
+    cardapio = df_cardapios.loc[df_cardapios['restaurant_id'] == row['id']]
+
+    cardapio = cardapio.drop(columns=['restaurant_id'])
+
+    # print(cardapio.columns.to_list())
+
+    for indice, linha in cardapio.iterrows():
+        # print (linha)
+        item = {
+            'categoria': linha['category'],
+            'nome': linha['name'],
+            'descricao': linha['description'],
+            'preco': linha['price']
+        }
+
+        cardapio_convertido.append(item)
+
+    # print(cardapio.head())
+
+    restaurante = {
+        # 'id': row['id'],
+        'posicao': row['position'],
+        'nome': row["name"],
+        'pontuacao': row["score"],
+        'avaliacoes': row['ratings'],
+        'categoria': row['category'],
+        'faixa_preco': row['price_range'],
+        'endereco': row['full_address'],
+        'codigo_zip': row['zip_code'],
+        'lat': row['lat'],
+        'lng': row['lng'],
+        'cardapio': cardapio_convertido
+    }
+
+    x = tabela_restaurantes.insert_one(restaurante)
+    
+    print(x)
+
