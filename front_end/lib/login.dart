955f483e9b4ae32621be05ce39c0000c2dd0944d
João Pedro Sanches Dovichi <jsanchesdovichi@gmail.com>
2024-11-27 15:03:28 -0300

    Roteador de front-end

---
+++ front_end/lib/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  /// Constructs a [MyApp]
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator();
+  }
+}
